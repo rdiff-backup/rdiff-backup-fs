dc9b32000475bef84a97190253cf120a7d24fc64
Filip Gruszczyński <gruszczy@gmail.com>
2011-02-05 14:49:00 +0100

    documenting some options of rdiff-backup-fs

--- parse.c
+++ parse.c
@@ -25,8 +25,6 @@
 #define OPT_CACHING "-c"
 #define OPT_CACHING_FULL "--caching"
 
-#define OPT_NO_CACHING_FULL "--no-caching"
-
 #define OPT_DIR "-d"
 #define OPT_DIR_FULL "--directory"
 
@@ -167,8 +165,6 @@ void parse_option(struct file_system_info *fsinfo, int argc, char **argv, int *i
     	if (set_directory(argc, argv, index) != 0)
     		fail(ERR_PARAMETRES);
     }
-    else if ((strcmp(argv[*index], OPT_NO_CACHING_FULL) == 0))
-        cache_limit = 0;
     else if ((strcmp(argv[*index], OPT_VERSION) == 0) || (strcmp(argv[*index], OPT_VERSION_FULL) == 0)) {
 		printf(PROGRAM_NAME " - filesystem in userspace for rdiff-backup repositories; version %s\n", PACKAGE_VERSION);
         exit(0);
